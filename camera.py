Photogrammetric filtering example barebone completed, normalisation functions moved to the filtering example, methods for getting point3D id and number of points3D in the reconstruction. Fixed bug in get_feature_names.

camera.py
@@ -137,7 +137,7 @@ class Camera:
     ''' ************************************************ Getters ************************************************ '''
     def get_id(self):
         return self.__id
-    
+
     def get_image_resolution(self):
         ''' Return image resolution (width and height).
 
@@ -148,7 +148,7 @@ class Camera:
             logger.critical('Image resolution not set')
             exit(1)
         return self.__intrinsics['width'], self.__intrinsics['height']
-    
+
     def get_camera_matrix_opencv(self):
         ''' Return a camera matrix with the instrinsic parameters using opencv format
 
@@ -167,7 +167,7 @@ class Camera:
                 [0, self.__intrinsics['f'], self.__intrinsics['cy']], 
                 [0, 0, 1]
             ], dtype=np.float)
-    
+
     def get_radial_distortion_params(self):
         ''' Return radial distortion parameters.
 
@@ -179,7 +179,7 @@ class Camera:
         else:
             logger.critical('Radial distortion parameters are not set')
             exit(1)
-    
+
     def get_tangential_distortion_params(self):
         ''' Return tangential distortion parameters
 
@@ -196,7 +196,7 @@ class Camera:
         ''' Returns the instrinsic distortion attributes in a distortion vector following the conventions of opencv
         '''
         return np.array([self.__intrinsics['k1'], self.__intrinsics['k2']], dtype=np.float)
-    
+
     def get_rotation_matrix(self, format):
         ''' Return the rotation matrix in the specified format
 
@@ -214,7 +214,7 @@ class Camera:
         else:
             logger.critical('{} is an invalid rotation matrix format'.format(format))
             exit(1)
-    
+
     def get_translation_vector(self, format):
         ''' Return the translation vector in the specified format
 

examples/photogrammetric_filtering.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import argparse
+import numpy as np
 
 # Path to geometry library
 sys.path.insert(0, '../')
@@ -9,14 +10,18 @@ sys.path.insert(0, '../')
 from geometry import Geometry
 from geometrySettings import GeometrySettings
 
-def import_sigma_features(path, geometry, have_header=True):
-    ''' Import in geometry the gamma feature. File format: x y z std_x std_y std_z std.
+# Global settings
+weight_by_multiplicity = False
+filtering_threshold = 0
+
+''' ************************************************ Import ************************************************ '''
+def import_sigma_features(path, geometry):
+    ''' Import in geometry the gamma feature exported by Metashape. File format: x y z std_x std_y std_z std.
         First row is the header.
 
         Attributes:
             path (string)           :   path to the file containing the sigma features
             geometry (Geometry)     :   reference to the geometry instance
-            have_header (bool)      :   specify if the file has a header
     '''
     if not os.path.isfile(path):
         logging.critical('File "{}" does not exist'.format(path))
@@ -26,25 +31,94 @@ def import_sigma_features(path, geometry, have_header=True):
     try:
         with open(path, 'r') as f:
             lines = f.readlines()
-            
-            if have_header:
-                start_index = 1
-            else:
-                start_index - 0
-            
-            for line in lines[start_index:]:
+                        
+            for p3D_id, line in enumerate(lines[1:]):
                 tokens = line.replace('\t', ' ').strip().split(' ')
                 if len(tokens) < 8:
                     logging.critical('Invalid instrinsic file format found at line {}. ' \
                         'Supported file format: <id> <x> <y> <z> <std_x> <std_y> <std_z> <std>'.format(line))
                     exit(1)
 
-                sigma_features[int(tokens[0])] = float(tokens[7])
+                # Use line number as id because the Metashape id is weird. 
+                # ASSUMPTION: ordering in this file is equal to the ordering in the out/nvm file.
+                sigma_features[p3D_id] = float(tokens[4])               
     except IOError:
         logging.critical('File "{}" is not readable'.format(path))
         exit(1)
 
     geometry.import_feature('point3D', 'sigma', sigma_features)
+
+
+''' ************************************************ Statistics ************************************************ '''
+def feature_scaling_normalisation(value, min, max):
+        ''' Normalise a given value between 0 and 1 using the feature scaling method
+        
+        Args:
+            value (float)   :   value to normalise.
+            min (float)     :   minimum value in the data.
+            max (float)     :   maximum value in the data.
+        
+        Return:
+            result (float)  :   Normalised value 
+        '''
+        result = ((((value - min) * (1 - 0)) / float(max - min)))  
+        return result
+
+def logistic_normalisation(value, mean, std):
+    ''' Normalise a value between 0 and 1 using a logistic function (Mauro version)
+
+    Args:
+        value (float)   :   value to normalise.
+        mean (float)    :   mean of the data
+        std (float)     :   standard deviation of the data 
+    '''
+    x = (2*(value - mean)) / std
+    return 1 / (1 + np.exp(-x))
+
+
+''' ************************************************ Filtering ************************************************ '''
+def compute_point3D_score(p3D, feature_stats):
+    ''' Computes the score of each point3D as a multiplicity-weighted (optional) summation of reprojection_error, maximum 
+        intersection angle, multiplicity and sigma. 
+
+        Attributes:
+            p3D (Point3D)                                       :   target point3D   
+            feature_stats ({string -> {string -> float}})       :   statistics of each point3D feature {feature -> {statistic -> value}}   
+        
+        Return:
+            score (float)                                       :   score of the given point3D
+    '''
+    score = 0
+    score += logistic_normalisation(p3D.get_feature('mean_reprojection_error'), feature_stats['mean_reprojection_error']['mean'], 
+        feature_stats['mean_reprojection_error']['std'])
+    score += logistic_normalisation(p3D.get_feature('sigma'), feature_stats['sigma']['mean'], feature_stats['sigma']['std'])
+    score += 1 - logistic_normalisation(p3D.get_feature('multiplicity'), feature_stats['multiplicity']['mean'], 
+        feature_stats['multiplicity']['std'])
+    score += 1 - logistic_normalisation(p3D.get_feature('max_intersec_angle'), feature_stats['max_intersec_angle']['mean'], 
+        feature_stats['max_intersec_angle']['std'])
+    print('p3D_id:{} - score:{}'.format(p3D.get_id(), score))
+
+    if weight_by_multiplicity:
+        return (p3D.get_feature('multiplicity') / feature_stats['multiplicity']['max']) * score
+    else:
+        return score
+
+def filter_points3D(geometry):
+    ''' Apply photogrammetric filtering to the points3D (and corresponding point2D) of the given reconstruction.
+
+        Attributes:
+            geometry (Geometry)     :   reference to the geometry instance
+    '''
+    p3Ds_feature_names = geometry.get_feature_names('point3D')
+    p3Ds_feature_stats = geometry.compute_feature_statistics(p3Ds_feature_names)
+
+    p3Ds_to_delete = []
+    for p3D_id in range(0, geometry.get_number_of_points3D()):
+        p3D = geometry.get_point3D(p3D_id)
+        if compute_point3D_score(p3D, p3Ds_feature_stats) > filtering_threshold:
+            p3Ds_to_delete.append(p3D_id)
+    logging.info('Filtering: deleted points3D {}'.format(len(p3Ds_to_delete)))
+
 
 def main():
     parser = argparse.ArgumentParser(description='Photogrammetric filtering of sparse reconstructions')
@@ -52,9 +126,9 @@ def main():
     parser.add_argument('--output', help='Path to the output folder', required=True)
     parser.add_argument('--intrinsics', help='Path to the file containing the full instrisic values of the cameras', required=True)
     parser.add_argument('--intrinsic_format', help='Format of the instrisic file', required=True)
+    parser.add_argument('--sigma', help='Path to the sigma features file', required=True)
     parser.add_argument('--threshold', help='Filtering equation delete threshold', type=float, required=True)
     parser.add_argument('--weight_by_multiplicity', help='Use multiplicity weighting in the filtering equation', type=bool, default=False)
-    parser.add_argument('--sigma', help='Path to the sigma features file', required=True)
     parser.add_argument('--debug', help='Run in debug mode', type=int, default=0)
     args = parser.parse_args()
 
@@ -65,8 +139,13 @@ def main():
     logging.basicConfig(format='%(levelname)-6s %(asctime)s:%(msecs)d [%(filename)s:%(lineno)d] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S', filename=os.path.join(args.output," log.txt"), filemode='w', level=log_level)
 
+    global weight_by_multiplicity, filtering_threshold
+    weight_by_multiplicity = args.weight_by_multiplicity
+    filtering_threshold = args.threshold
+    logging.info('Filtering params: use multiplicity as weigth: {}, filtering threshold: {}'.format(weight_by_multiplicity, filtering_threshold))
+
     geometry = Geometry()
-    
+
     geometry.load_reconstruction(args.input)
     if args.intrinsic_format.lower() == 'metashape':
         geometry.load_full_camera_intrinsics(args.intrinsics, GeometrySettings.InstriscsFormatType.METASHAPE)
@@ -75,9 +154,15 @@ def main():
     else:
         logging.critical('Unknown intrinsic format. Supported values: [\'opencv\', \'metashape\']')
         exit(1)
-    
+
+    geometry.compute_mean_reprojection_errors()
+    geometry.compute_multiplicities()
+    geometry.compute_max_intersection_angles()
     import_sigma_features(args.sigma, geometry)
-    geometry.export_points3D_xyz_and_features(args.output)
+
+    filter_points3D(geometry)
+
+    # geometry.export_points3D_xyz_and_features(args.output)
 
 
 if __name__ == '__main__':

geometry.py
@@ -249,6 +249,11 @@ class Geometry:
             return self.__points3D[p3D_id]
         else:
             logger.error('Point3D with id {} does not exist'.format(p3D_id))
+    
+    def get_number_of_points3D(self):
+        ''' Return the number of points3D in the reconstruction
+        '''
+        return len(self.__points3D)
 
     def get_camera(self, cam_id):
         ''' Return the camera with the given id.
@@ -270,10 +275,10 @@ class Geometry:
             Return:
                 features [string]       :   features
         '''
-        if type in self.__features.keys():
-            return self.__features[type].copy()
+        if feature_type in self.__features.keys():
+            return self.__features[feature_type].copy()
         else:
-            logger.critical('Type {} is not a valid feature type'.format(type))
+            logger.critical('Type {} is not a valid feature type'.format(feature_type))
             exit(1)
     
 
@@ -434,7 +439,7 @@ class Geometry:
         
         logger.info('Reprojection errors computed')
 
-    def compute_max_intersection_angle(self, in_degree=False):
+    def compute_max_intersection_angles(self, in_degree=False):
         ''' Compute the maximum intersection angle of all the points3D.
 
             Attributes:
@@ -476,8 +481,8 @@ class Geometry:
 
         logger.info('Maximum intersection angles computed')
 
-    def compute_multiplicity(self):
-        ''' Compute the multiplicity (number of cameras seing the point) of all the points3D.
+    def compute_multiplicities (self):
+        ''' Compute the multiplicity (number of cameras seing each point3D).
         '''
         if len(self.__points3D) == 0:
             logger.critical('No points3D, no multiplicities')
@@ -493,31 +498,6 @@ class Geometry:
 
 
     ''' ************************************************ Statistic ************************************************ '''
-    def feature_scaling_normalisation(self, value, min, max):
-        ''' Normalise a given value between 0 and 1 using the feature scaling method
-        
-        Args:
-            value (float)   :   value to normalise.
-            min (float)     :   minimum value in the data.
-            max (float)     :   maximum value in the data.
-        
-        Return:
-            result (float)  :   Normalised value 
-        '''
-        result = ((((value - min) * (1 - 0)) / float(max - min)))  
-        return result
-
-    def logistic_normalisation(self, value, mean, std):
-        ''' Normalise a value between 0 and 1 using a logistic function (Mauro version)
-
-        Args:
-            value (float)   :   value to normalise.
-            mean (float)    :   mean of the data
-            std (float)     :   standard deviation of the data 
-        '''
-        x = (2*(value - mean)) / std
-        return 1 / (1 + np.exp(-x))
-
     def compute_feature_statistics(self, features, ids_to_exclude=None):
         ''' Compute feature statistics of all the points3D or of a specif set (when ids_to_exclude is specified)
 
@@ -565,21 +545,15 @@ class Geometry:
            return
         
         try:
-            features_names = self.get_feature_names('points3D')
+            features_names = self.get_feature_names('point3D')
             with open(os.path.join(folder, 'pwf.txt'), 'w') as f_out:
-                f_out.write('#id x y z {}reprojection_error multiplicity max_angle\n'.format(' '.join(features_names)))
+                f_out.write('#id x y z {}\n'.format(' '.join(features_names)))
 
                 for p3D_id, p3D in self.__points3D.items():
                     f_out.write('{} {}'.format(p3D_id, p3D.get_coordinates_as_string()))
                     for feature_name in features_names:
                         f_out.write(' {}'.format(p3D.get_feature(feature_name)))
                     f_out.write('\n')
-                    '''f_out.write('{} {} {} {} {}\n'.format(p3D_id, 
-                        p3D.get_coordinates_as_string(), 
-                        p3D.get_feature('mean_reprojection_error'),
-                        p3D.get_feature('multiplicity'),
-                        p3D.get_feature('max_intersec_angle'))
-                    )'''
         except IOError:
             logger.error('Cannot create file {}"'.format(os.path.join(folder, 'pwf.txt')))
             exit(1)

point3D.py
@@ -75,6 +75,11 @@ class Point3D:
 
 
     ''' ************************************************ Getters ************************************************ '''
+    def get_id(self):
+        ''' Return the id of the point3D.
+        '''
+        return self.__id
+        
     def get_coordinates(self, homogeneous = False):
         ''' Return the 3D coordinates of the point3D.
 
@@ -90,7 +95,7 @@ class Point3D:
             return np.copy(self.__xyz)
 
     def get_coordinates_as_string(self):
-        ''' Return the 3D cordinates as a string "x y z"
+        ''' Return the 3D cordinates as a string "x y z".
 
             Return
                 xyz (string)    :   coordinates as string